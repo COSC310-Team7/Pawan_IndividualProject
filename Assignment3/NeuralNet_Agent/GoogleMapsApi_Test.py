884fe49221c9a2c62c71ea8ea846eb367a5cb218
Pawan <pbilkhu15@gmail.com>
2021-03-29 12:38:19 -0700

    Removed API key from GoogleMapsApi_Test.py

--- Assignment3/NeuralNet_Agent/GoogleMapsApi_Test.py
+++ Assignment3/NeuralNet_Agent/GoogleMapsApi_Test.py
@@ -2,7 +2,7 @@ import googlemaps
 import time
 
 # Define API key
-API_KEY = "AIzaSyBQ-29eV8FoXD5DQQTt8gGxudchujLiLgU"
+API_KEY = "Place holder for key"
 
 # Define client
 gmaps = googlemaps.Client(key=API_KEY)
